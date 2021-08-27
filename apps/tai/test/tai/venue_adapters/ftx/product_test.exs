6971f98314e0357a2cddd7098bdd32aa9440386d
alex <alex+git@fremantle.io>
2021-08-27 13:46:13 -0700

    WIP: Set product type for unique FTX products
    - leveraged token
    - move
    - bvol
    
    TODO:
    
    - Test
    - Implement
    - Move logic out of products and into product so that it's unit tested

---
+++ apps/tai/test/tai/venue_adapters/ftx/product_test.exs
@@ -0,0 +1,18 @@
+defmodule Tai.VenueAdapters.Ftx.ProductTest do
+  use ExUnit.Case, async: false
+
+  test ".build/2 can parse futures products" do
+  end
+
+  test ".build/2 can parse swap products" do
+  end
+
+  test ".build/2 can parse leveraged_token products" do
+  end
+
+  test ".build/2 can parse move products" do
+  end
+
+  test ".build/2 can parse bvol products" do
+  end
+end
